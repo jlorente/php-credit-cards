2a8a24b55b242f2202a610930c91fa55b89e6f78
Jose Lorente <jose.lorente.martin@gmail.com>
2023-09-07 13:29:14 +0200

    Fixed return value type in matches function

--- src/CreditCardTypeConfig.php
+++ src/CreditCardTypeConfig.php
@@ -228,11 +228,11 @@ class CreditCardTypeConfig
     public function matches($cardNumber)
     {
         if ($this->matchesLengths($cardNumber) === false) {
-            return 0;
+            return false;
         }
 
         if ($this->getLuhnCheck() && $this->satisfiesLuhn($cardNumber) === false) {
-            return 0;
+            return false;
         }
 
         return $this->matchesPatterns($cardNumber);
